e87e6da3de7db3101b4c921710427adccede95ef
Vitaliy Saliy <vitaliy.saliy@rinftech.com>
2019-01-13 18:35:38 +0200

    sample Spring Boot Serverless project

---
+++ src/main/java/com/mtdevuk/SpringLambdaUrlShorterApplication.java
@@ -0,0 +1,14 @@
+package com.mtdevuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLambdaUrlShorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLambdaUrlShorterApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/mtdevuk/SpringLambdaUrlShorterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mtdevuk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLambdaUrlShorterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
